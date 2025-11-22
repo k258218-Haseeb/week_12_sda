03b48dd1aaca67b41dc50bb94a2b3a7e6be2c7c7
k258218-Haseeb <k258218@nu.edu.pk>
2025-11-22 19:37:57 +0500

    Cpu.java

---
+++ Cpu.java
@@ -0,0 +1,6 @@
+package lab.facade;
+public class Cpu {
+public void freeze() { System.out.println("CPU: freeze"); }
+public void jump(long position) { System.out.println("CPU: jump to " + position); }
+public void execute() { System.out.println("CPU: execute"); }
+}
